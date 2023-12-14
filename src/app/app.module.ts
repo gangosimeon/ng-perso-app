f670169d170592685ff60672bcadf50cdf4c22e5
Gango Simeon <gangosimeon@gmail.com>
2023-12-14 16:00:05 +0000

    Import dans AppModule resgisterLocaleDato pour Currency symbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import LocaleFr from '@angular/common/locales/fr';
+registerLocaleData(LocaleFr);
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     CommonModule
   ],
-  providers: [],
+  providers: [
+    {provide:'LOCALE_ID', useValue:'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
